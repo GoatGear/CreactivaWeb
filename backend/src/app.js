823600963c940050f8e2036536148c547e80dc0d
GoatGear <edgrdoierr@gmail.com>
2024-06-28 13:59:47 -0600

    wtf

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
-import authRoutes from './routes/auth.routes.js';
-import taskRoutes from './routes/task.routes.js';
+// import authRoutes from './routes/auth.routes.js';
+// import taskRoutes from './routes/task.routes.js';
 
 
 const app = express();
